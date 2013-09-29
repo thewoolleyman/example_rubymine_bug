937f9b2f6fe80a85114ac9529e652a9d475cf072
Chad Woolley <thewoolleyman@gmail.com>
2013-09-29 09:24:04 -0700

    make use of example_for annotation, so intended usage is clearer

--- sample_spec.rb
+++ sample_spec.rb
@@ -6,7 +6,7 @@ require 'spec_helper'
 require 'sample'
 
 describe Sample do
-  it "should return 1" do
+  it "should return 1", :example_for => 'spec annotation' do
     Sample.new.one.should == 1
   end
 end
