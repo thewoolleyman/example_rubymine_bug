f21c7c09e453071584f2ca1b223ce77aa2cc6686
Chad Woolley <thewoolleyman@gmail.com>
2013-09-29 09:45:23 -0700

    work around bug by overriding and duplicating #example method without aliasing.

--- spec_helper.rb
+++ spec_helper.rb
@@ -1,37 +1,14 @@
 require 'spec'
 
 module Spec::Example::ExampleGroupMethods
-  def self.alias_method_chain(target, feature)
-    # Strip out punctuation on predicates or bang methods since
-    # e.g. target?_without_feature is not a valid method name.
-    aliased_target, punctuation = target.to_s.sub(/([?!=])$/, ''), $1
-    yield(aliased_target, punctuation) if block_given?
-
-    with_method, without_method = "#{aliased_target}_with_#{feature}#{punctuation}", "#{aliased_target}_without_#{feature}#{punctuation}"
-
-    alias_method without_method, target
-    alias_method target, with_method
-
-    case
-      when public_method_defined?(without_method)
-        public target
-      when protected_method_defined?(without_method)
-        protected target
-      when private_method_defined?(without_method)
-        private target
-    end
-  end
-
-  unless method_defined?(:example_without_hook)
-    def example_with_hook(description=nil, options={}, backtrace=nil, &implementation)
-      if options.has_key? :example_for
-        puts "----- in hook, description = #{description}, options = #{options.inspect}"
-      end
-
-      example_without_hook(description, options, backtrace, &implementation)
+  def it(description=nil, options={}, backtrace=nil, &implementation)
+    if options.has_key? :example_for
+      puts "----- in hook, description = #{description}, options = #{options.inspect}"
     end
 
-    alias_method_chain :example, :hook
-    alias_method :it, :example_with_hook
+    example_proxy = Spec::Example::ExampleProxy.new(description, options, backtrace || caller(0)[1])
+    example_proxies << example_proxy
+    example_implementations[example_proxy] = implementation || pending_implementation
+    example_proxy
   end
 end
